1e17d686506af69241d783cbf1d676b118fcfbd2
a5 <a5@LS3-PC06>
2017-03-15 14:29:21 -0600

    Avance 1

---
+++ EP4_POO2017_AxelMartinez_LuisSoto/src/ep4_poo2017_axelmartinez_luissoto/EP4_POO2017_AxelMartinez_LuisSoto.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ep4_poo2017_axelmartinez_luissoto;
+
+/**
+ *
+ * @author a5
+ */
+public class EP4_POO2017_AxelMartinez_LuisSoto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
